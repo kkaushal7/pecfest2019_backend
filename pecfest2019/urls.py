2a0afdfa95a999be18254b7874419e1011467c39
unknown <madhavpruthi.becse16@pec.edu.in>
2019-05-19 17:24:11 +0530

    Fix path import

--- pecfest2019/urls.py
+++ pecfest2019/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url, include, path
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
+from django.urls import path
 from django.views.generic import TemplateView
 from views import home
 
@@ -9,7 +10,7 @@ urlpatterns = [
     url(r'^login/$', TemplateView.as_view(template_name='login.html'), name='login'),
     # url(r'^logout/$', views.logout, name='logout'),
     url(r'^auth/', include('social_django.urls', namespace='social')),
-    url(r'^$', home, name='home'),
+    #url(r'^$', home, name='home'),
     path('', include('frontend.urls')),
     path('', include('backend.urls'))
 ]
