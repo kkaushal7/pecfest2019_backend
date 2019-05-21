39379c5b294b1524e1671da6f8c76d4557f8c627
unknown <madhavpruthi.becse16@pec.edu.in>
2019-05-22 00:40:22 +0530

    removed unnecessary links

--- pecfest2019/urls.py
+++ pecfest2019/urls.py
@@ -6,12 +6,11 @@ from django.views.generic import TemplateView
 from views import home
 
 urlpatterns = [
+
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', TemplateView.as_view(template_name='login.html'), name='login'),
-    # url(r'^logout/$', views.logout, name='logout'),
     url(r'^auth/', include('social_django.urls', namespace='social')),
-    #url(r'^$', home, name='home'),
     path('', include('frontend.urls')),
     path('', include('backend.urls'))
+    
 ]
 
