0f1a8d68cd681580a716476863ae28c15e35b117
unknown <madhavpruthi.becse16@pec.edu.in>
2019-05-27 00:17:55 +0530

    update req.txt

--- pecfest2019/settings.py
+++ pecfest2019/settings.py
@@ -47,7 +47,14 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'knox.auth.TokenAuthentication',
+    ),
+
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
 
 MIDDLEWARE = [
