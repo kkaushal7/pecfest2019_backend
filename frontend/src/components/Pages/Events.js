7bc890ff0e0e3223cbc88ec29f2e692583f1bd62
kartik kaushal <t-kartik.kaushal@ocltp.com>
2019-05-30 15:02:35 +0530

    First

--- frontend/src/components/Pages/Events.js
+++ frontend/src/components/Pages/Events.js
@@ -1,13 +1,57 @@
-import React, {Component} from 'react';
+import React, { Fragment } from "react";
+import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from
+"mdbreact";
 
-class Events extends Component {
-    render() {
-        return (
-            <h1>
-                This is Events
-            </h1>
-        );
-    }
+import './EventsPage.scss';
+const Events = () => {
+  return (
+    <Fragment>  
+    <div className="eventsPage">
+    <div className="carousel">
+    <MDBContainer>
+      <MDBCarousel
+      activeItem={1}
+      length={2}
+      showControls={true}
+      showIndicators={true}
+      className="z-depth-1"
+    >
+      <MDBCarouselInner>
+        <MDBCarouselItem itemId="1">
+          <MDBView>
+            <img
+              className="d-block w-100"
+              src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+              alt="First slide"
+            />
+          <MDBMask overlay="black-light" />
+          </MDBView>
+          <MDBCarouselCaption>
+            <h3 className="h3-responsive">Light mask</h3>
+            <p>First text</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+        <MDBCarouselItem itemId="2">
+          <MDBView>
+            <img
+              className="d-block w-100"
+              src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg"
+              alt="Second slide"
+            />
+          <MDBMask overlay="black-strong" />
+          </MDBView>
+          <MDBCarouselCaption>
+            <h3 className="h3-responsive">Strong mask</h3>
+            <p>Second text</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+      </MDBCarouselInner>
+    </MDBCarousel>
+    </MDBContainer>
+    </div>
+    </div>
+    </Fragment>
+  );
 }
 
 export default Events;
